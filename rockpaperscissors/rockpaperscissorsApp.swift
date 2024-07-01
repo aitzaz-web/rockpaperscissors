3e4acaede4f625f680fb820c5b147763c8f74de7
Aitzaz Munir Shaikh <ams845@cornell.edugit config --global user.email ams845@cornell.edu>
2024-07-01 14:58:46 +0500

    Initial Commit

---
+++ rockpaperscissors/rockpaperscissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockpaperscissorsApp.swift
+//  rockpaperscissors
+//
+//  Created by Aitzaz Munir on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct rockpaperscissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
